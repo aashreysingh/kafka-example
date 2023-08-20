f777e56caa58219461b3fc864fd1c79f6d69a2fc
Aashrey Singh <aashreyofficial@gmail.com>
2023-08-20 22:42:00 +0530

    added bean to configure command line runner to send message to a topic

--- src/main/java/com/bizarre/kafkaexample/KafkaExampleApplication.java
+++ src/main/java/com/bizarre/kafkaexample/KafkaExampleApplication.java
@@ -1,13 +1,24 @@
 package com.bizarre.kafkaexample;
 
+import com.bizarre.kafkaexample.constants.ConfigConstants;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class KafkaExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaExampleApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
+		return args -> {
+			kafkaTemplate.send(ConfigConstants.GENERIC_TOPIC_NAME, "Hello Kafka!!");
+		};
 	}
 
 }
