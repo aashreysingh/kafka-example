0999a23e5871ee97a220e428915c4502021f4401
Aashrey Singh <aashreyofficial@gmail.com>
2023-08-20 23:33:45 +0530

    added logic to handle post request and send/produce an event to kafka which is being consumed by Consumer

--- src/main/java/com/bizarre/kafkaexample/controller/MessageController.java
+++ src/main/java/com/bizarre/kafkaexample/controller/MessageController.java
@@ -1,7 +1,24 @@
 package com.bizarre.kafkaexample.controller;
 
+import com.bizarre.kafkaexample.constants.ConfigConstants;
+import com.bizarre.kafkaexample.request.MessageRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/v1/messages")
 public class MessageController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping
+    public void publish(@RequestBody MessageRequest messageRequest){
+        kafkaTemplate.send(ConfigConstants.GENERIC_TOPIC_NAME, messageRequest.message());
+    }
+
 }
