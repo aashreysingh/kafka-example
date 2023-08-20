d1b5a4eda83a1a8bce31b3f6e85841f6556dd422
Aashrey Singh <aashreyofficial@gmail.com>
2023-08-20 23:19:20 +0530

    added support to consume the data and print it in the console

--- src/main/java/com/bizarre/kafkaexample/listeners/KafkaListeners.java
+++ src/main/java/com/bizarre/kafkaexample/listeners/KafkaListeners.java
@@ -1,7 +1,18 @@
 package com.bizarre.kafkaexample.listeners;
 
+import com.bizarre.kafkaexample.constants.ConfigConstants;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaListeners {
+
+    @KafkaListener(
+            topics = ConfigConstants.GENERIC_TOPIC_NAME,
+            groupId = "groupId"
+    )
+    void listener(String data){
+        System.out.println("Listener Received: " + data + " 🎉");
+    }
+
 }
