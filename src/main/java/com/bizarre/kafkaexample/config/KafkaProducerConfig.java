fa03adccb1c0928ed6351f81c8ff2d60c02bb0e3
Aashrey Singh <aashreyofficial@gmail.com>
2023-08-20 20:25:47 +0530

    added configuration for Key, Value Serializers & Kafka Bootstrap servers on producer

--- src/main/java/com/bizarre/kafkaexample/config/KafkaProducerConfig.java
+++ src/main/java/com/bizarre/kafkaexample/config/KafkaProducerConfig.java
@@ -1,7 +1,25 @@
 package com.bizarre.kafkaexample.config;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootsrapServers;
+
+    public Map<String, Object> producerConfig(){
+        HashMap<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootsrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return props;
+    }
 }
